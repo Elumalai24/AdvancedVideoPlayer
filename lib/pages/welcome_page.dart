faa66003e06dba6593a2a270c1e942124ea56655
ElumalaiJayamoorthy <elumalai.j@glueblu.com>
2024-07-05 18:20:43 +0530

    video player fetched

---
+++ lib/pages/welcome_page.dart
@@ -0,0 +1,98 @@
+import 'dart:io';
+
+import 'package:advanced_video_player/constants.dart';
+
+import 'package:advanced_video_player/pages/normal_player_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
+
+class WelcomePage extends StatefulWidget {
+  @override
+  _WelcomePageState createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  @override
+  void initState() {
+    _saveAssetSubtitleToFile();
+    _saveAssetVideoToFile();
+    _saveAssetEncryptVideoToFile();
+    _saveLogoToFile();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return NormalPlayerPage();
+  }
+
+  List<Widget> buildExampleElementWidgets() {
+    return [
+      _buildExampleElementWidget("Normal player", () {
+        _navigateToPage(NormalPlayerPage());
+      }),
+    ];
+  }
+
+  Widget _buildExampleElementWidget(String name, Function onClicked) {
+    return Material(
+      child: InkWell(
+        onTap: onClicked as void Function()?,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8),
+              child: Text(
+                name,
+                style: TextStyle(fontSize: 16),
+              ),
+            ),
+            Divider(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future _navigateToPage(Widget routeWidget) {
+    return Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => routeWidget),
+    );
+  }
+
+  ///Save subtitles to file, so we can use it later
+  Future _saveAssetSubtitleToFile() async {
+    String content =
+        await rootBundle.loadString("assets/example_subtitles.srt");
+    final directory = await getApplicationDocumentsDirectory();
+    var file = File("${directory.path}/example_subtitles.srt");
+    file.writeAsString(content);
+  }
+
+  ///Save video to file, so we can use it later
+  Future _saveAssetVideoToFile() async {
+    var content = await rootBundle.load("assets/testvideo.mp4");
+    final directory = await getApplicationDocumentsDirectory();
+    var file = File("${directory.path}/testvideo.mp4");
+    file.writeAsBytesSync(content.buffer.asUint8List());
+  }
+
+  Future _saveAssetEncryptVideoToFile() async {
+    var content =
+        await rootBundle.load("assets/${Constants.fileTestVideoEncryptUrl}");
+    final directory = await getApplicationDocumentsDirectory();
+    var file = File("${directory.path}/${Constants.fileTestVideoEncryptUrl}");
+    file.writeAsBytesSync(content.buffer.asUint8List());
+  }
+
+  ///Save logo to file, so we can use it later
+  Future _saveLogoToFile() async {
+    var content = await rootBundle.load("assets/${Constants.logo}");
+    final directory = await getApplicationDocumentsDirectory();
+    var file = File("${directory.path}/${Constants.logo}");
+    file.writeAsBytesSync(content.buffer.asUint8List());
+  }
+}
